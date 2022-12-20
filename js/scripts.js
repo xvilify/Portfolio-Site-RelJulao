efd542d9ff61ddd0eed15163cfa2adb8a758c8aa
xvilify <119160163+xvilify@users.noreply.github.com>
2022-12-19 22:38:44 -0800

    2.3

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,13 @@
 
 console.log("Unsheathe your dagger definitions.")
 //.log will show on //
+
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
